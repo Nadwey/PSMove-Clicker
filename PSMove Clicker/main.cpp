fb307c0287b2c4edc4ecf2ec9533d0693672402a
Nadwey <81181783+Nadwey@users.noreply.github.com>
2022-03-14 14:32:27 +0100

    Update
     - Changed color system
     - Fixed bug when using cursor

--- PSMove Clicker/main.cpp
+++ PSMove Clicker/main.cpp
@@ -3,9 +3,6 @@
 #include <iostream>
 #include <unordered_map>
 #include <sstream>
-#include <future>
-#include <chrono>
-#include <thread>
 
 #pragma comment(lib, "Comctl32.lib")
 #pragma comment(lib, "PSMoveClient_CAPI.lib")
@@ -20,25 +17,24 @@
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #endif
 
+struct RGB {
+	unsigned char r;
+	unsigned char g;
+	unsigned char b;
+};
+
+struct controllerInfo {
+	PSMController* controller;
+	RGB color;
+};
+
 PSMControllerList controllerList = { 0 };
 std::unordered_map<int, PSMVector3f> speeds;
-std::unordered_map<int, PSMController*> controllers;
+std::unordered_map<int, controllerInfo> controllers;
 PSMVector3f VEL = { -4.0f, 0.0f, 0.0f };
 int refreshRateVal = 100;
 UINT_PTR timerID = 0;
 NW::UI::TextBoxMultiline* logTextbox;
-int lightTime = 100;
-
-template <typename... ParamTypes>
-void setTimeOut(int milliseconds, std::function<void(ParamTypes...)> func, ParamTypes... parames)
-{
-	std::thread{
-	[=]() {
-		std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
-		func(parames...);
-	}
-	}.detach();
-};
 
 void rebuildControllerList()
 {
@@ -58,16 +54,6 @@ void uninitializeController(int id)
 	PSM_FreeControllerListener(controllerList.controller_id[id]);
 }
 
-void OnEvent(PSMEventMessage* msg) {
-	switch (msg->event_type)
-	{
-	case PSMEventMessage::PSMEvent_connectedToService: {
-		
-		break;
-	}
-	}
-}
-
 void InfoTimer(HWND, UINT, UINT_PTR, DWORD)
 {
 	
@@ -75,7 +61,7 @@ void InfoTimer(HWND, UINT, UINT_PTR, DWORD)
 	{
 		std::wstringstream wss;
 		for (int i = 0; i < controllerList.count; i++) {
-			PSMController* controller = controllers[i];
+			PSMController* controller = controllers[i].controller;
 
 			std::wstring batteryText;
 			if (controller->ControllerState.PSMoveState.BatteryValue == PSMBattery_Charging) batteryText = L"£adowanie...";
@@ -92,6 +78,11 @@ void InfoTimer(HWND, UINT, UINT_PTR, DWORD)
 	}
 }
 
+void setControllerColor(int id, unsigned char r, unsigned char g, unsigned char b)
+{
+	controllers[id].color = { r, g, b };
+}
+
 void MainTimer(HWND, UINT, UINT_PTR, DWORD)
 {
 	if (PSM_GetIsInitialized())
@@ -99,28 +90,12 @@ void MainTimer(HWND, UINT, UINT_PTR, DWORD)
 		PSM_Update();
 
 		// Poll any events from the service
-		
-
-		// Poll events queued up by the call to ClientPSMoveAPI::update()
-		//PSMMessage message;
-		//while (PSM_PollNextMessage(&message, sizeof(message)) == PSMResult_Success)
-		//{
-		//	switch (message.payload_type)
-		//	{
-		//	case PSMMessage::_messagePayloadType_Response:
-		//		//onClientPSMoveResponse(&message.response_data);
-		//		break;
-		//	case PSMMessage::_messagePayloadType_Event:
-		//		//OnEvent(&message.event_data);
-		//		break;
-		//	}
-		//}
 
 		if (PSM_HasControllerListChanged())
 		{
 			for (int i = 0; i < controllerList.count; i++) {
 				uninitializeController(i);
-				controllers[i] = nullptr;
+				controllers.erase(i);
 			}
 
 			rebuildControllerList();
@@ -128,26 +103,42 @@ void MainTimer(HWND, UINT, UINT_PTR, DWORD)
 			for (int i = 0; i < controllerList.count; i++)
 			{
 				initializeController(i, PSMControllerDataStreamFlags::PSMStreamFlags_defaultStreamOptions | PSMControllerDataStreamFlags::PSMStreamFlags_includeCalibratedSensorData);
-				controllers[i] = PSM_GetController(controllerList.controller_id[i]);
+				controllers[i] = { nullptr, {0, 0, 0} };
+				controllers[i].controller = PSM_GetController(controllerList.controller_id[i]);
 			}
 		}
 
 		for (int i = 0; i < controllerList.count; i++)
 		{
-			PSMController* controller = controllers[i];
+			controllerInfo& controllerInf = controllers[i];
+			PSMController* controller = controllerInf.controller;
 			PSMPSMoveCalibratedSensorData& calibsens = controller->ControllerState.PSMoveState.CalibratedSensorData;
 			
-			std::function<void(int)> turnOffCallback = [](int i) {
-				PSM_SetControllerLEDOverrideColor(controllerList.controller_id[i], 0, 0, 0);
-			};
+			PSM_SetControllerLEDOverrideColor(controllerList.controller_id[i], controllerInf.color.r, controllerInf.color.g, controllerInf.color.b);
 
-			auto turnOffLight = [i, turnOffCallback]() {
-				if (lightTime == 0) return;
-				else setTimeOut<int>(lightTime, turnOffCallback, i);
-			};
+
+			if (controller->ControllerState.PSMoveState.CircleButton == PSMButtonState_PRESSED)
+			{
+				setControllerColor(i, 0, 0, 255);
+				INPUT Inputs[2] = { 0 };
+				Inputs[0].type = INPUT_MOUSE;
+				Inputs[0].mi.dwFlags = MOUSEEVENTF_RIGHTDOWN;
+				Inputs[1].type = INPUT_MOUSE;
+				Inputs[1].mi.dwFlags = MOUSEEVENTF_RIGHTUP;
+				SendInput(2, Inputs, sizeof(INPUT));
+			}
+
+			if (controller->ControllerState.PSMoveState.SelectButton == PSMButtonState_PRESSED)
+			{
+				setControllerColor(i, 255, 0, 0);
+				keybd_event(VK_ESCAPE, 0, 0, 0);              // press the Esc key
+				keybd_event(VK_ESCAPE, 0, KEYEVENTF_KEYUP, 0); // let up the Esc key
+			}
 
 			if (controller->ControllerState.PSMoveState.TriggerValue > 200)
 			{
+				setControllerColor(i, 0, 255, 0);
+
 				INPUT Input = { 0 };
 				Input.type = INPUT_MOUSE;
 
@@ -157,46 +148,25 @@ void MainTimer(HWND, UINT, UINT_PTR, DWORD)
 				Input.mi.dwFlags = MOUSEEVENTF_MOVE;
 				SendInput(1, &Input, sizeof(INPUT));
 
-
 				break;
 			}
 
-
 			if (calibsens.Gyroscope.x < VEL.x && speeds[i].x > VEL.x) {
-				PSM_SetControllerLEDOverrideColor(controllerList.controller_id[i], 255, 30, 255);
+				setControllerColor(i, 255, 255, 255);
 				INPUT Inputs[2] = { 0 };
 				Inputs[0].type = INPUT_MOUSE;
 				Inputs[0].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
 				Inputs[1].type = INPUT_MOUSE;
 				Inputs[1].mi.dwFlags = MOUSEEVENTF_LEFTUP;
 				SendInput(2, Inputs, sizeof(INPUT));
-
-				turnOffLight();
 			}
-			if (controller->ControllerState.PSMoveState.CircleButton == PSMButtonState_PRESSED)
-			{
-				PSM_SetControllerLEDOverrideColor(controllerList.controller_id[i], 30, 255, 255);
-				INPUT Inputs[2] = { 0 };
-				Inputs[0].type = INPUT_MOUSE;
-				Inputs[0].mi.dwFlags = MOUSEEVENTF_RIGHTDOWN;
-				Inputs[1].type = INPUT_MOUSE;
-				Inputs[1].mi.dwFlags = MOUSEEVENTF_RIGHTUP;
-				SendInput(2, Inputs, sizeof(INPUT));
 
-				turnOffLight();
-			}
-			if (controller->ControllerState.PSMoveState.SelectButton == PSMButtonState_PRESSED)
-			{
-				PSM_SetControllerLEDOverrideColor(controllerList.controller_id[i], 255, 0, 0);
-				keybd_event(VK_ESCAPE, 0, 0, 0);              // press the Esc key
-				keybd_event(VK_ESCAPE, 0, KEYEVENTF_KEYUP, 0); // let up the Esc key
-
-				turnOffLight();
-			}
 			speeds[i] = calibsens.Gyroscope;
+			const unsigned char minus = 2047.0f / refreshRateVal;
+			controllerInf.color.r = static_cast<unsigned char>(controllerInf.color.r - minus) > controllerInf.color.r ? 0 : static_cast<unsigned char>(controllerInf.color.r - minus);
+			controllerInf.color.g = static_cast<unsigned char>(controllerInf.color.g - minus) > controllerInf.color.g ? 0 : static_cast<unsigned char>(controllerInf.color.g - minus);
+			controllerInf.color.b = static_cast<unsigned char>(controllerInf.color.b - minus) > controllerInf.color.b ? 0 : static_cast<unsigned char>(controllerInf.color.b - minus);
 		}
-
-		
 	}
 }
 
@@ -212,13 +182,6 @@ void refreshRefreshRateText(NW::UI::TextBoxSingleline* refreshRate)
 	std::wstringstream wss;
 	wss << L"Odúwieøanie (" << refreshRateVal << L")";
 	refreshRate->SetPlaceholder(wss.str());
-}
-
-void refreshLightTimeText(NW::UI::TextBoxSingleline* lightTimeTextbox)
-{
-	std::wstringstream wss;
-	wss << L"Czas migniÍcia (" << lightTime << L")";
-	lightTimeTextbox->SetPlaceholder(wss.str());
 }
 
 int main()
@@ -284,32 +247,13 @@ int main()
 
 	};
 
-	NW::UI::TextBoxSingleline lightTimeTextbox(&mainWindow, NW::UI::Position(5, 65, 200, 25), L"");
-	refreshLightTimeText(&lightTimeTextbox);
-	NW::UI::Button lightTimeApply(&mainWindow, NW::UI::Position(210, 65, 150, 25), L"Potwierdü");
-	lightTimeApply.EventHandler = [&](NW::UI::ControlEventTypes eventType, NW::UI::ControlEventInfo* eventInfo) {
-		switch (eventType)
-		{
-		case NW::UI::ControlEventTypes::FromParent_Command:
-		{
-			try {
-				lightTime = std::stoi(lightTimeTextbox.GetText());
-			}
-			catch (...) {}
 
-			lightTimeTextbox.SetText(L"");
 
-			refreshLightTimeText(&lightTimeTextbox);
-			break;
-		}
-		}
-	};
+
 
 	NW::UI::TextBoxMultiline logLoc(&mainWindow, NW::UI::Position(365, 5, 400, 400), L"");
 	logLoc.SetReadOnly(true);
 	logTextbox = &logLoc;
-
-
 	mainWindow.Show();
 
 
@@ -323,7 +267,8 @@ int main()
 	for (int i = 0; i < controllerList.count; i++)
 	{
 		initializeController(i, PSMControllerDataStreamFlags::PSMStreamFlags_defaultStreamOptions | PSMControllerDataStreamFlags::PSMStreamFlags_includeCalibratedSensorData);
-		controllers[i] = PSM_GetController(controllerList.controller_id[i]);
+		controllers[i] = { nullptr, {0, 0, 0} };
+		controllers[i].controller = PSM_GetController(controllerList.controller_id[i]);
 	}
 
 	WPARAM result = app.MessageLoop();
